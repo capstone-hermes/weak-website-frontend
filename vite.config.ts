5a856124adc91415194b64cc873e06d2fc348f42
valentinmaurel0 <valentin.maurel@orange.fr>
2024-12-04 18:53:06 +0100

    fix: Update Vite configuration to include environment variables and server settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 8081
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const PORT = Number(env.VITE_CLIENT_PORT) || 3000;
+
+  return {
+    plugins: [react()],
+    server: {
+      port: PORT,
+      strictPort: true,
+      host: true,
+      cors: true,
+    }
   }
 })
